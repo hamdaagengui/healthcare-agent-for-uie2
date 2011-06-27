7a2101a9148d65e9bf65654555999285d98a6440
ch.heiming <ch.heiming@googlemail.com>
2011-06-27 23:35:58 +0000

    Clean Project...

---
+++ MangeYourPatients/src/de/uie2/healthcare/MainActivity.java
@@ -0,0 +1,13 @@
+package de.uie2.healthcare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
